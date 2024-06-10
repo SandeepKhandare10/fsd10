9b7bf58c203d98f1abd2388d21561916db133f73
SandeepKhandare10 <sandipkhandare10@gmail.com>
2024-06-10 19:10:13 +0530

    sk_commit

--- Main.java
+++ Main.java
@@ -1,5 +1,13 @@
 import java.util.*;
-new code
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+//        String name = sc.nextLine();
+//        System.out.println(name);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = a+b;
+        System.out.print(sum);
 
     }
 }
